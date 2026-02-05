919f466fdfdbdeee1b1890f338077a3563864979
Bria Smith <bzs0144@auburn.edu>
2026-02-05 13:18:07 -0600

    changes

--- PLPA6820_DataVisualizationNotes.R
+++ PLPA6820_DataVisualizationNotes.R
@@ -1,11 +1,12 @@
 #### PLPA Class Notes - Data Visualization ####
-Objective is to learn about data plotting using ggplot2
+# Objective is to learn about data plotting using ggplot2
 
 install.packages("ggplot2") # didn't have it installed so here
 library(ggplot2)
 
 mtcars
 ggplot(mtcars, aes(x=wt, y=mpg)) +
-  geom_point()
+  geom_point() +
+  geom_smooth()
 # aes - aesthetic; setting the x and y
 # layers are connected w p
